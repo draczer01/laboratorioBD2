25f59d8c40232daf1d29f27a088aba327848c89d
draczer01 <31465304+draczer01@users.noreply.github.com>
2018-04-28 14:06:09 -0500

    Add files via upload

---
+++ practica4.sql
@@ -0,0 +1,191 @@
+
+select * from empleados;
+select * from establo;
+select * from persona;
+select * from caballo;
+-------------------------------------inicio insert de establos-------------------------------------
+insert into establo
+values(
+1,20,'mexico',1
+);
+insert into establo
+values(
+2,20,'puebla',2
+);
+insert into establo
+values(
+3,20,'NuevoLeon',3
+);
+insert into establo
+values(
+4,20,'zacatecas',4
+);
+insert into establo
+values(
+5,20,'guanajuato',5
+);
+insert into establo
+values(
+6,20,'florida',6
+);
+insert into establo
+values(
+7,20,'guerrero',7
+);
+insert into establo
+values(
+8,20,'baja california norte',8
+);
+insert into establo
+values(
+9,20,'baja california sur',9
+);
+insert into establo
+values(
+10,20,'xochimilco',10
+);
+
+-------------------------------------fin de insert a establos------------------------------------------
+
+-------------------------------------inicio de insert de empleados-------------------------------------
+insert into empleados
+values(
+1,1,'Jose',200.00,'gerente'
+);
+insert into empleados
+values(
+2,2,'Arturo',200.00,'gerente'
+);
+insert into empleados
+values(
+3,3,'Antonio',200.00,'gerente'
+);
+insert into empleados
+values(
+4,4,'Rodrigo',200.00,'gerente'
+);
+insert into empleados
+values(
+5,5,'Humberto',200.00,'gerente'
+);
+insert into empleados
+values(
+6,6,'Alejadra',200.00,'gerente'
+);
+insert into empleados
+values(
+7,7,'Andrea',200.00,'gerente'
+);
+insert into empleados
+values(
+8,8,'Edgar',200.00,'gerente'
+);
+insert into empleados
+values(
+9,9,'Gabriel',200.00,'gerente'
+);
+insert into empleados
+values(
+10,10,'Samanta',200.00,'gerente'
+);
+-------------------------------------fin de inserts de empleados-------------------------------------
+
+-------------------------------------inicio de inserts de persona------------------------------------
+
+insert into persona
+values(
+1,'Oscar','aguas calientes',1
+);
+insert into persona
+values(
+2,'Marcela','aguas frias',2
+);
+insert into persona
+values(
+3,'Hector','chiuaua',3
+);
+insert into persona
+values(
+4,'Mario','apodaca',4
+);
+insert into persona
+values(
+5,'Luis','San pedro',5
+);
+insert into persona
+values(
+6,'Andres','achipinque',6
+);
+insert into persona
+values(
+7,'Daniel','guadalajara',7
+);
+insert into persona
+values(
+8,'Milton','India',8
+);
+insert into persona
+values(
+9,'Ninfa','Juston',9
+);
+insert into persona
+values(
+10,'Christian','Australia',10
+);
+
+-------------------------------------fin de inserts de persona------------------------------------
+-------------------------------------inicio de inserts de caballos--------------------------------
+
+insert into caballo
+values(
+1,'tinto','13k/h','corintio',6,500000.00,1,1
+);
+insert into caballo
+values(
+2,'negro','27k/h','Rufus',10,150000.00,2,2
+);
+insert into caballo
+values(
+3,'ruso','30k/h','secretarias',6,200000.00,3,3
+);
+insert into caballo
+values(
+4,'español','23k/h','bowjak',6,30000.00,4,4
+);
+insert into caballo
+values(
+5,'real','25k/h','spirit',6,300000.00,5,5
+);
+insert into caballo
+values(
+6,'fake','15k/h','suki',6,250000.00,6,6
+);
+insert into caballo
+values(
+7,'hypo','19k/h','mechas',6,330000.00,7,7
+);
+insert into caballo
+values(
+8,'mustang','26k/h','bucefalo',6,12000.00,8,8
+);
+insert into caballo
+values(
+9,'sonic','32k/h','rayo',6,70000.00,9,9
+);
+insert into caballo
+values(
+10,'rupestre','34k/h','kachaw',6,60000.00,10,10
+);
+-------------------------------------------------------updates------------------------------------------------
+update
+update
+update
+update
+update
+-------------------------------------------------------deleats------------------------------------------------
+
+delete
+delete
+delete
+delete
+delete
