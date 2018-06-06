e7613ef0c1e7b32600ca7e28c94ca80b8f800a92
draczer01 <31465304+draczer01@users.noreply.github.com>
2018-06-06 18:28:17 -0500

    Add files via upload

---
+++ practica 7.sql
@@ -0,0 +1,56 @@
+use [caballos]
+
+create view 
+datos_persona
+as select idpersona, nombre, correo, direccion
+from persona; 
+select * from datos_persona
+
+create view
+datos_ubicacion
+as select idubicacion, estado, ciudad
+from ubicacion;
+select * from datos_direccion
+
+create view 
+datos_trabajador
+as select idtrabajador, nombre, salario,idestablo
+from trabajador;
+select * from datos_trabajador
+
+create view 
+datos_establo
+as select idestablo, ubicacion
+from establo;
+select * from datos_establo
+
+create view 
+datos_caballo
+as select caballo, velocidad, nombre, raza
+from caballo;
+select * from datos_telefonos
+
+
+select persona.Idpersona, ubicacion.estado
+from persona 
+inner join ubicacion on persona.direccion = ubicacion.Idubicacion;
+
+select trabajador.nombre, establo.idestablo
+from trabajador
+left join idestablo on trabajador.Idtrabajador = establo.Idestablo
+order by trabajador.nombre;
+
+select establo.nombre, trabajador.nombre
+from establo
+right join trabajador on establo.Idestabloes = trabajador.Idtrabajador
+order by establo.nombre 
+
+select * from persona
+left join trabajador on persona.Idpersona = trabajador.Idtrabajador
+union 
+select * from persona
+right join trabajador on persona.Idpersona = trabajador.Idtrabajador
+
+select establo.Idestabloes, establo.nombre
+from establo
+cross join ubicacion;
