4f2405d6ac888732e7923eeb4627b280cf0dcc13
draczer01 <31465304+draczer01@users.noreply.github.com>
2018-06-06 20:13:11 -0500

    Update practica 8.sql

--- practica 8.sql
+++ practica 8.sql
@@ -37,4 +37,28 @@ as
 print 'hubo un cambio'
 go
 
+CREATE FUNCTION Comprado(@id_caballo numeric(18, 0))
+RETURNS @persona TABLE
+(
+id_persona numeric(18, 0), nombre VARCHAR(50),direccion VARCHAR(50) ,id_caballo numeric(18, 0)
+)
+AS
+BEGIN
+INSERT @persona SELECT id_persona,nombre,direccion,id_caballo FROM persona
+WHERE id_caballo=@id_caballo
+RETURN
+END
+go
 
+CREATE FUNCTION quesoso(@id_cempleado numeric(18, 0))
+RETURNS @establo TABLE
+(
+id_establo numeric(18, 0), espacio VARCHAR(50),ubicacion VARCHAR(50) ,id_empleado numeric(18, 0)
+)
+AS
+BEGIN
+INSERT @establo SELECT id_establo,espacio,ubicacion,id_empleado FROM establo
+WHERE id_empleado=@id_cempleado
+RETURN
+END
+go
